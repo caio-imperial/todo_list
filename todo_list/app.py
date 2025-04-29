193715cd7bd41978e4b33a6b63d0e80520a2dd31
caio.imperial <scaioimperial@gmail.com>
2025-04-29 20:49:12 -0300

    criado a primeira rota de pegar tarefas

--- todo_list/app.py
+++ todo_list/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
+lista_de_tarefas = []
+
+tarefa = {
+    "titulo": "estudar",
+    "descricao": "estudar python",
+    "feito": False
+}
+
+lista_de_tarefas.append(tarefa)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return redirect('/api/tarefas')
+
+@app.route("/api/tarefas", methods=['GET'])
+def ler_tarefas():
+    return lista_de_tarefas
